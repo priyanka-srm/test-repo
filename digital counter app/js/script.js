e187d91dab923cb75c18feb7ba7b9ebcb044821e
priyanka-srm <priyankasql.co@gmail.com>
2026-03-26 18:33:22 +0530

    added digital counter app

---
+++ digital counter app/js/script.js
@@ -0,0 +1,17 @@
+let count = 0;
+let countValue = document.getElementById("count");
+let incBtn = document.getElementById("increase");
+let decBtn = document.getElementById("decrease");
+let resetBtn = document.getElementById("reset");
+incBtn.addEventListener("click", function(){
+    count++;
+    countValue.textContent = count;
+});
+decBtn.addEventListener("click", function(){
+    count--;
+    countValue.textContent = count;
+});
+resetBtn.addEventListener("click", function(){
+    count = 0;
+    countValue.textContent = count;
+});
